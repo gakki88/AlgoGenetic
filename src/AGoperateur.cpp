805fa442ca1d44a7f5a51e0a17ca458db325e85e
gakki88 <tangtang5yo@gmail.com>
2018-02-09 00:28:56 +0800

    ajouter les methodes pour emploi du temps

--- src/AGoperateur.cpp
+++ src/AGoperateur.cpp
@@ -73,6 +73,75 @@ int AGoperateur::mutation_robot(int chemin[]){
 	return chemin;
 }
 
+vector<chemin> AGoperateur::CreerPopulation(){
+	vector<chemin> pop;
+	vector<int> myVec;
+	chemin c;
+
+	for(int n=0;n<100;n++){
+
+		for (int i=1; i<28; ++i) myVec.push_back(i);
+		random_shuffle ( myVec.begin(), myVec.end());
+		for (std::vector<int>::iterator it=myVec.begin(); it!=myVec.end(); ++it)
+		    	c.insert((int) *it);
+
+		pop.push_back(c);
+		c.clear();
+
+	}
+
+	return pop;
+}
+
+vector<chemin> AGoperateur::selection(vector<chemin> pop){
+	vector<chemin> selected;
+	int randIndex;
+
+	for (int i=0;i<9;i++){
+
+		randIndex = rand()%100;
+		selected.push_back(pop[randIndex]);
+
+	}
+
+	return selected;
+
+}
+
+chemin AGoperateur::croisement_EdT(chemin c1,chemin c2){
+	chemin cEnfant;
+	int randIndex;
+
+	randIndex = rand()%27;
+
+
+	for(int i=0;i<randIndex;i++){
+		cEnfant.insert(c1.showdata(i));
+		for(int j=0;j<27;j++){
+			if(c2.showdata(j)==c1.showdata(i)){
+				c2.erase(j);
+			}
+		}
+	}
+
+	concat(cEnfant,c2);
+
+	return cEnfant;
+}
+
+void AGoperateur::mutation_EdT(chemin c){
+	int r1,r2;
+
+	r1=rang()%27;
+	r2=rand()%27;
+
+	c.insert_pos(c.showdata(r2),r1);
+	c.insert_pos(c.showdata(r1+1),r2+1);
+	c.erase(r1+1);
+	c.erase(r2+1);
+}
+
+
 /*int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
