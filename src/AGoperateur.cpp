034c02f0d271e7036e39b26edd0e5ef2a0357ba6
virgiliaL <jianglei.wang@insa-rouen.fr>
2018-02-09 18:27:38 +0100

    一波关于robot的大更新

--- src/AGoperateur.cpp
+++ src/AGoperateur.cpp
@@ -11,66 +11,99 @@
 #include "AGoperateur.h"
 #include <typeinfo>
 #include <string>
+#include <ctime>
 using namespace std;
 
-void AGoperateur::initialisation_robot(float pc,float pm,int tp,int n,robot r,int p[][]){
+void AGoperateur::initialisation_robot(float pc,float pm,int tp,int n,robot r){
 	this->ProbaCrossover=pc;
 	this->ProbaMutate=pm;
 	this->TaillePop=tp;
 	this->N=n;
 	int a,b,i,j=1;
+	chemin p[tp];
+	srand(time(0));
 		for(i=0;i<tp;i++){
-			p[1][i]=r.getEntre();
+			p[i].insert(r.getEntre());
 			a=rand()%99;
+			if(a!=0){
 			do{
 				do{
-				p[j][i]=rand()%100;
-				}while(r.isObstacles(p[j][i])==true&&p[j][i]<p[j-1][i]);
+				b=rand()%100+1;
+				}while(r.isObstacles(b)==true);
+				p[i].insert(b);
 				j++;
-			}while(j<a);
-			p[a][i]=r.getSortie();
-			for(j=a;j<101;j++){
-				p[j][i]=NULL;
+			}while(j<=a);
 			}
+			p[i].insert(r.getSortie());
+
 		}
 	this->pop=p;
 }
 
-float AGoperateur::fitness_robot(int chemin[]){
+float AGoperateur::fitness_robot(int a){
 	int i=0;
-	float fitness,distance=0,f[];
+	float fitness,distance=0,f[2];
 	do{
-		distance=sqrt(pow((int)(chemin[i+1]/10)-(int)(chemin[i]/10),2)+pow(chemin[i+1]%10-chemin[i]%10,2))+distance;
+		distance=sqrt(pow((int)(pop[a].showdata(i+1)-1/10)-(int)(pop[a].showdata(i)-1/10),2)+pow(pop[a].showdata(i+1)-1%10-pop[a].showdata(i)-1%10,2))+distance;
 		i++;
-	}while(chemin[i+1]==NULL|i==99);
-	fitness=(1+1/sqrt(i-1))*distance;
-	f[0]=distance;
-	f[1]=fitness;
+	}while(pop[a].showdata(i+1)!=0|i!=99);
+	fitness=(1+1/sqrt(i))/distance;
+	f[0]=dis[a]=distance;
+	f[1]=fit[a]=fitness;
+
 	return f;
 }
 
-bool AGoperateur::suppression(int chemin[],robot r){
+int AGoperateur::selection_robot(float f){
+	int i,b[TaillePop+1],p,t=0;
+	float sum=0;
+	b[0]=0;
+	for(i=0;i<TaillePop;i++){
+		sum=sum+fit[i];
+	}
+	for(i=0;i<TaillePop;i++){
+		b[i+1]=b[i]+fit[i]/sum;
+	}
+	srand(time(NULL));
+	p=rand()/float(RAND_MAX);
+	while(p>b[t]){
+		t++;
+	}
+	return t-1;
+}
+
+
+bool AGoperateur::suppression(int a){
 	bool b=false;
 	int i=0,j=1;
 	do{
 		do{
-			if (chemin[i]==chemin[j]){
+			if (pop[a].showdata(i)==pop[a].showdata(j)){
 				b=true;
 			}
 			else {
 				j++;
 			}
-		}while(chemin[j]==NULL|b==true);
+		}while(pop[a].showdata!=0|b==true);
 		i++;
-	}while(b==true|chemin[i+1]==NULL);
+	}while(b==true|pop[a].showdata(i+1)!=0);
+
 	return b;
 }
 
-int AGoperateur::mutation_robot(int chemin[]){
-	if (rand()<ProbaMutate){
-		chemin[rand()%sizeof(chemin)]=rand()%100;
+chemin AGoperateur::mutation_robot(int a){
+	srand(time(0));
+	int i=0,p;
+	do{
+	if (rand()/float(RAND_MAX)<ProbaMutate){
+		do{
+			p=rand()%100+1;
+			pop[a].setData(i,p);
+		}while(suppression(i));
 	}
-	return chemin;
+	i++;
+	}while(pop[a].showdata(i)!=0);
+	return pop;
 }
 
 vector<chemin> AGoperateur::CreerPopulation(){
