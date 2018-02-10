9dfda80bd3203fd5d31dafc7f4397f3389d6f82d
virgiliaL <jianglei.wang@insa-rouen.fr>
2018-02-10 17:52:19 +0100

    add some methode

--- src/gene.cpp
+++ src/gene.cpp
@@ -1,7 +1,7 @@
 /*
  * gene.cpp
  *
- *  Created on: 2018Äê1ÔÂ29ÈÕ
+ *  Created on: 2018Ã„Ãª1Ã”Ã‚29ÃˆÃ•
  *      Author: virgiliaL
  */
 
@@ -28,6 +28,70 @@ bool robot::isObstacles(int chemin){
 		}
 	}while(b==true|i==sizeof(obstacles)+1);
 	return b;
+}
+
+bool robot::estVoisin(int a,int b){
+	if(a==b+10|a==b-10|a==b+1|a==b-1){
+		return true;
+	}
+	else{
+		return false;
+	}
+
+}
+
+int robot::max(int a,int b){
+	if (a<b){
+		return b;
+	}
+	else{
+		return a;
+	}
+}
+
+int robot::min(int a,int b){
+	if(a<b){
+		return a;
+	}
+	else{
+		return b;
+	}
+}
+
+bool robot::passObstacles(int p1,int p2){
+	bool bo=false;
+	int ob[][],i=0;
+	int ox1,ox2,ox3,ox4,oy1,oy2,oy3,oy4;
+	float x1,x2,y1,y2,a,b;
+	x1=p1%10+0.5;
+	x2=p2%10+0.5;
+	y1=(int)p1/10+0.5;
+	y2=(int)p2/10+0.5;
+	a=(y1-y2)/(x1-x2);
+	b=y1-a*x1;
+	do{
+		if((int)obstacles[i]/10+0.5<=max(y1,y2)&&
+				(int)obstacles[i]/10+0.5>=min(y1,y2)&&
+				obstacles[i]%10+0.5<=max(x1,x2)&&
+				obstacles[i]%10+0.5>=min(x1,x2)&&!estVoisin(obstacles[i],a)&&!estVoisin(obstacles[i],b)){
+
+			ox1=(int)obstacles[i]/10;
+			ox2=ox1;
+			ox3=ox1+1;
+			ox4=ox3;
+			oy1=obstacles[i]%10;
+			oy2=oy1+1;
+			oy3=oy1;
+			oy4=oy2;
+			if((a*ox1+b-oy1)*(a*ox2+b-oy2)<0||(a*ox1+b-oy1)*(a*ox3+b-oy3)<0||(a*ox1+b-oy1)*(a*ox4+b-oy4)<0){
+				bo=true;
+			}
+		}
+		i++;
+
+	}while(bo==true|i==sizeof(obstacles));
+		return bo;
+
 }
 
 int robot::getEntre(){
